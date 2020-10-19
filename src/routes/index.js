920790c802b11a1660853665cbcd84e9e269221d
duane-faria <duane.faria@etec.sp.gov.br>
2020-10-19 19:28:30 -0300

    Criando funcionalidade de protected route, alterando o manifest e o index.html

--- src/routes/index.js
+++ src/routes/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import Login from '../views/Login';
 import Orders from '../views/Orders';
+import ProtectedRoute from './ProtectedRoute';
 function routes() {
   return (
     <Switch>
       <Route exact path='/' component={Login}></Route>
-      <Route exact path='/pedidos' component={Orders}></Route>
+      <ProtectedRoute exact path='/pedidos' component={Orders}></ProtectedRoute>
     </Switch>
   );
 }
